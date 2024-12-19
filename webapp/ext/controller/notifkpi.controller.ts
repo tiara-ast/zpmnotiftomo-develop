23a2f348355ae7d3b6afe7b6ec40761402f49a6d
tiara-ast <ta.astari@gmail.com>
2024-12-19 20:01:21 +0700

    Develop9

--- webapp/ext/controller/notifkpi.controller.ts
+++ webapp/ext/controller/notifkpi.controller.ts
@@ -30,52 +30,7 @@ export default {
         }
     },
 
-    getBasicParams(oNavigateParams, oSelectionVariantParams: SelectionVariant): object[] {
-
-        const basicParams = ['Werks', 'Auart', 'Ingpr']
-        const params = []
-
-        basicParams.forEach(b => {
-            const selParam = oSelectionVariantParams.getSelectOption(b)
-
-            if (oNavigateParams[b]) {
-                params.push({
-                    path: b,
-                    operator: 'EQ',
-                    value1: oNavigateParams[b],
-                    value2: '',
-                    sign: 'I'
-                })
-
-            } else {
-                if (selParam) {
-                    selParam.forEach(p => {
-                        params.push({
-                            path: b,
-                            operator: p.Option,
-                            value1: p.Low,
-                            value2: '',
-                            sign: p.Sign
-                        })
-                    })
-                } else {
-                    params.push({
-                        path: b,
-                        operator: 'EQ',
-                        value1: '',
-                        value2: '',
-                        sign: 'I'
-                    })
-                }
-            }
-        })
-
-
-
-        return params
-    },
-
-    getBasicParams2(oNavigateParams, oSelectionVariantParams: SelectionVariant): object[] {
+    getbasicparams(oNavigateParams, oSelectionVariantParams: SelectionVariant): object[] {
 
         const basicParams = ['Werks', 'Auart', 'Ingpr']
         const params = []
@@ -120,16 +75,18 @@ export default {
     },
 
      paramThisMonthHRKPI(oNavigateParams, oSelectionVariantParams: SelectionVariant) {
-debugger
-        const params  = this.getBasicParams2(oNavigateParams, oSelectionVariantParams)
+
+        const params  = this.getbasicparams(oNavigateParams, oSelectionVariantParams)
         const SelDate = oSelectionVariantParams.getSelectOption("OrderCrtrd")
         var SelDate1 = SelDate[0].Low;
 
         const yearfr    = parseInt(SelDate1.substring(0, 4))
         const monthfr   = parseInt(SelDate1.substring(4, 6))
         const dayfr     = parseInt(SelDate1.substring(6, 8))
-
-        const DateFr = new Date(yearfr, monthfr, dayfr)
+        debugger
+        const DateFr = new Date(yearfr, monthfr - 1, dayfr)
+        //const tzOffsetNumber = DateFrPre.getTimezoneOffset();
+       // const DateFr = new Date(yearfr, monthfr - 1, dayfr,)
 
         var SelDate2 = SelDate[0].High;
 
@@ -137,7 +94,7 @@ debugger
         const monthto   = parseInt(SelDate2.substring(4, 6))
         const dayto     = parseInt(SelDate2.substring(6, 8))
 
-        const DateTo = new Date(yearto, monthto, dayto)
+        const DateTo = new Date(yearto, monthto - 1, dayto)
         const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
         params.push({
              path: 'DateFr',
@@ -160,157 +117,192 @@ debugger
 
      paramLastMonthHRKPI(oNavigateParams, oSelectionVariantParams: SelectionVariant) {
 
-         const params = this.getBasicParams2(oNavigateParams, oSelectionVariantParams)
-         const SelDate = oSelectionVariantParams.getParameter('OrderCrtrd')
+        const params  = this.getbasicparams(oNavigateParams, oSelectionVariantParams)
+        const SelDate = oSelectionVariantParams.getSelectOption("OrderCrtrd")
+        var SelDate1 = SelDate[0].Low;
 
-         const year = parseInt(SelDate.substring(0, 4))
-         const month = parseInt(SelDate.substring(5, 7))
-         const day = parseInt(SelDate.substring(8, 10))
- 
-         const DateFr = new Date(year, month, day)
-         const DateTo = new Date(year, month, day)
-         const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
- 
-         params.push({
+        const yearfr    = parseInt(SelDate1.substring(0, 4))
+        const monthfr   = parseInt(SelDate1.substring(4, 6))
+        const dayfr     = parseInt(SelDate1.substring(6, 8))
+
+        const DateFr = new Date(yearfr, monthfr -1, dayfr)
+
+        var SelDate2 = SelDate[0].High;
+
+        const yearto    = parseInt(SelDate2.substring(0, 4))
+        const monthto   = parseInt(SelDate2.substring(4, 6))
+        const dayto     = parseInt(SelDate2.substring(6, 8))
+
+        const DateTo = new Date(yearto, monthto - 1, dayto)
+        const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
+        params.push({
              path: 'DateFr',
              operator: 'EQ',
              value1: formatter.format(DateFr),
              value2: '',
              sign: 'I'
-         })
- 
-         params.push({
-             path: 'DateTo',
-             operator: 'EQ',
-             value1: formatter.format(DateTo),
-             value2: '',
-             sign: 'I'
-         })
- 
+          })
+
+          params.push({
+              path: 'DateTo',
+              operator: 'EQ',
+              value1: formatter.format(DateTo),
+              value2: '',
+              sign: 'I'
+          })
+
          return params
      },
 
      paramThisMonthAVGKPI(oNavigateParams, oSelectionVariantParams: SelectionVariant) {
 
-         const params = this.getBasicParams2(oNavigateParams, oSelectionVariantParams)
-         const SelDate = oSelectionVariantParams.getParameter('OrderCrtrd')
+        const params  = this.getbasicparams(oNavigateParams, oSelectionVariantParams)
+        const SelDate = oSelectionVariantParams.getSelectOption("OrderCrtrd")
+        var SelDate1 = SelDate[0].Low;
 
-         const year = parseInt(SelDate.substring(0, 4))
-         const month = parseInt(SelDate.substring(5, 7))
-         const day = parseInt(SelDate.substring(8, 10))
- 
-         const DateFr = new Date(year, month, day)
-         const DateTo = new Date(year, month, day)
-         const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
- 
-         params.push({
+        const yearfr    = parseInt(SelDate1.substring(0, 4))
+        const monthfr   = parseInt(SelDate1.substring(4, 6))
+        const dayfr     = parseInt(SelDate1.substring(6, 8))
+
+        const DateFr = new Date(yearfr, monthfr - 1, dayfr)
+
+        var SelDate2 = SelDate[0].High;
+
+        const yearto    = parseInt(SelDate2.substring(0, 4))
+        const monthto   = parseInt(SelDate2.substring(4, 6))
+        const dayto     = parseInt(SelDate2.substring(6, 8))
+
+        const DateTo = new Date(yearto, monthto - 1, dayto)
+        const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
+        params.push({
              path: 'DateFr',
              operator: 'EQ',
              value1: formatter.format(DateFr),
              value2: '',
              sign: 'I'
-         })
- 
-         params.push({
-             path: 'DateTo',
-             operator: 'EQ',
-             value1: formatter.format(DateTo),
-             value2: '',
-             sign: 'I'
-         })
- 
+          })
+
+          params.push({
+              path: 'DateTo',
+              operator: 'EQ',
+              value1: formatter.format(DateTo),
+              value2: '',
+              sign: 'I'
+          })
          return params
      },
      paramLastMonthAVGKPI(oNavigateParams, oSelectionVariantParams: SelectionVariant) {
 
-         const params = this.getBasicParams2(oNavigateParams, oSelectionVariantParams)
-         const SelDate = oSelectionVariantParams.getParameter('OrderCrtrd')
+        const params  = this.getbasicparams(oNavigateParams, oSelectionVariantParams)
+        const SelDate = oSelectionVariantParams.getSelectOption("OrderCrtrd")
+        var SelDate1 = SelDate[0].Low;
 
-         const year = parseInt(SelDate.substring(0, 4))
-         const month = parseInt(SelDate.substring(5, 7))
-         const day = parseInt(SelDate.substring(8, 10))
- 
-         const DateFr = new Date(year, month, day)
-         const DateTo = new Date(year, month, day)
-         const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
- 
-         params.push({
+        const yearfr    = parseInt(SelDate1.substring(0, 4))
+        const monthfr   = parseInt(SelDate1.substring(4, 6))
+        const dayfr     = parseInt(SelDate1.substring(6, 8))
+
+        const DateFr = new Date(yearfr, monthfr - 1, dayfr)
+
+        var SelDate2 = SelDate[0].High;
+
+        const yearto    = parseInt(SelDate2.substring(0, 4))
+        const monthto   = parseInt(SelDate2.substring(4, 6))
+        const dayto     = parseInt(SelDate2.substring(6, 8))
+
+        const DateTo = new Date(yearto, monthto - 1, dayto)
+        const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
+        params.push({
              path: 'DateFr',
              operator: 'EQ',
              value1: formatter.format(DateFr),
              value2: '',
              sign: 'I'
-         })
- 
-         params.push({
-             path: 'DateTo',
-             operator: 'EQ',
-             value1: formatter.format(DateTo),
-             value2: '',
-             sign: 'I'
-         })
- 
+          })
+
+          params.push({
+              path: 'DateTo',
+              operator: 'EQ',
+              value1: formatter.format(DateTo),
+              value2: '',
+              sign: 'I'
+          })
+
          return params
      },
      paramDateRangeAvgKPI(oNavigateParams, oSelectionVariantParams: SelectionVariant) {
+        const params  = this.getbasicparams(oNavigateParams, oSelectionVariantParams)
+        const SelDate = oSelectionVariantParams.getSelectOption("OrderCrtrd")
+        var SelDate1 = SelDate[0].Low;
 
-        const params = this.getBasicParams2(oNavigateParams, oSelectionVariantParams)
-        const SelDate = oSelectionVariantParams.getParameter('OrderCrtrd')
+        const yearfr    = parseInt(SelDate1.substring(0, 4))
+        const monthfr   = parseInt(SelDate1.substring(4, 6))
+        const dayfr     = parseInt(SelDate1.substring(6, 8))
 
-        const year = parseInt(SelDate.substring(0, 4))
-        const month = parseInt(SelDate.substring(5, 7))
-        const day = parseInt(SelDate.substring(8, 10))
+        const DateFr = new Date(yearfr, monthfr - 1, dayfr)
 
-        const DateFr = new Date(year, month, day)
-        const DateTo = new Date(year, month, day)
+        var SelDate2 = SelDate[0].High;
+
+        const yearto    = parseInt(SelDate2.substring(0, 4))
+        const monthto   = parseInt(SelDate2.substring(4, 6))
+        const dayto     = parseInt(SelDate2.substring(6, 8))
+
+        const DateTo = new Date(yearto, monthto - 1, dayto)
         const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
-
         params.push({
-            path: 'DateFr',
-            operator: 'EQ',
-            value1: formatter.format(DateFr),
-            value2: '',
-            sign: 'I'
-        })
+             path: 'DateFr',
+             operator: 'EQ',
+             value1: formatter.format(DateFr),
+             value2: '',
+             sign: 'I'
+          })
 
-        params.push({
-            path: 'DateTo',
-            operator: 'EQ',
-            value1: formatter.format(DateTo),
-            value2: '',
-            sign: 'I'
-        })
+          params.push({
+              path: 'DateTo',
+              operator: 'EQ',
+              value1: formatter.format(DateTo),
+              value2: '',
+              sign: 'I'
+          })
+
 
         return params
     },
+
     paramDateRangeHrKPI(oNavigateParams, oSelectionVariantParams: SelectionVariant) {
         
-        const params = this.getBasicParams2(oNavigateParams, oSelectionVariantParams)
-        const SelDate = params.path["OrderCrtrd.value1"]
-        debugger
-        const year = parseInt(SelDate.substring(0, 4))
-        const month = parseInt(SelDate.substring(5, 7))
-        const day = parseInt(SelDate.substring(8, 10))
+        const params  = this.getbasicparams(oNavigateParams, oSelectionVariantParams)
+        const SelDate = oSelectionVariantParams.getSelectOption("OrderCrtrd")
+        var SelDate1 = SelDate[0].Low;
 
-        const DateFr = new Date(year, month, day)
-        const DateTo = new Date(year, month, day)
+        const yearfr    = parseInt(SelDate1.substring(0, 4))
+        const monthfr   = parseInt(SelDate1.substring(4, 6))
+        const dayfr     = parseInt(SelDate1.substring(6, 8))
+
+        const DateFr = new Date(yearfr, monthfr - 1, dayfr)
+
+        var SelDate2 = SelDate[0].High;
+
+        const yearto    = parseInt(SelDate2.substring(0, 4))
+        const monthto   = parseInt(SelDate2.substring(4, 6))
+        const dayto     = parseInt(SelDate2.substring(6, 8))
+
+        const DateTo = new Date(yearto, monthto - 1, dayto)
         const formatter = DateFormat.getDateInstance({ pattern: 'dd.MM.yyyy' })
-
         params.push({
-            path: 'DateFr',
-            operator: 'EQ',
-            value1: formatter.format(DateFr),
-            value2: '',
-            sign: 'I'
-        })
+             path: 'DateFr',
+             operator: 'EQ',
+             value1: formatter.format(DateFr),
+             value2: '',
+             sign: 'I'
+          })
 
-        params.push({
-            path: 'DateTo',
-            operator: 'EQ',
-            value1: formatter.format(DateTo),
-            value2: '',
-            sign: 'I'
-        })
+          params.push({
+              path: 'DateTo',
+              operator: 'EQ',
+              value1: formatter.format(DateTo),
+              value2: '',
+              sign: 'I'
+          })
 
         return params
     },
